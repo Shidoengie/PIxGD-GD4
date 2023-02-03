c738ad5d419b2b4f39c62b548e5f9b2c6fd56e85
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2023-02-03 17:21:05 +0000

    Added ways to manipulate the color and zoom
    sliders and stuff

--- Scenes/Canvas/src/Control.gd
+++ Scenes/Canvas/src/Control.gd
@@ -1,5 +1,6 @@
 extends Control
 
+signal zoom_changed(value)
 @export var canvas_size = Vector2(100,100)
 @onready var DrawSurface = get_node("%DrawSurface")
 @onready var CanvasCamera = $CanvasCamera
@@ -7,17 +8,26 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	DrawSurface.size = canvas_size
-	DrawControl.size = canvas_size
+	DrawControl.size = canvas_size+Vector2.ONE
 	$Container.size = canvas_size
 	$DrawControl/SubViewport.size = canvas_size
+
 func _input(event):
+	
 	if event is InputEventMouseButton and event.is_pressed():
 		match event.button_index:
 			MOUSE_BUTTON_WHEEL_UP:
 				CanvasCamera.zoom *= Vector2(1.5,1.5)
+				emit_signal("zoom_changed",CanvasCamera.zoom)
+				Global.canvas_zoom = CanvasCamera.zoom
 			MOUSE_BUTTON_WHEEL_DOWN:
 				CanvasCamera.zoom /= Vector2(1.5,1.5)
+				emit_signal("zoom_changed",CanvasCamera.zoom)
+				Global.canvas_zoom = CanvasCamera.zoom
 	if not event is InputEventMouseMotion:
 		return
 	if Input.is_action_pressed("mouseMiddle_3rd"):
 		CanvasCamera.position -= event.relative/CanvasCamera.zoom
+
+func update_zoom(value):
+	CanvasCamera.zoom = value
---
+++ src/Global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var canvas_zoom = Vector2.ONE
