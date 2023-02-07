a46adbf7c2db6ae12a5a1b125634b08fc16ce569
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2023-02-07 22:43:52 +0000

    improved checkers shader and started work on ui

--- Scenes/Canvas/src/Control.gd
+++ Scenes/Canvas/src/Control.gd
@@ -1,6 +1,8 @@
 extends Control
 
 signal zoom_changed(value)
+@export var primary_color = Color.BLACK
+@export var secondary_color = Color.WHITE
 @export var current_tool = BrushInfo.ToolEnum.PEN
 @export var canvas_size = Vector2(20,20)
 @onready var DrawSurface = get_node("%DrawSurface")
@@ -8,6 +10,8 @@ signal zoom_changed(value)
 @onready var DrawControl = $DrawControl
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	BrushInfo.primary_color = primary_color
+	BrushInfo.secondary_color = secondary_color 
 	DrawSurface.size = canvas_size
 	BrushInfo.current_tool = current_tool
 	DrawControl.size = canvas_size+Vector2.ONE
--- src/Global.gd
+++ src/Global.gd
@@ -1,3 +1,5 @@
 extends Node
 
 var canvas_zoom = Vector2.ONE
+var transCol1 :Color
+var transCol2 :Color
